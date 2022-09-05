c769d3149ba00d04a3874ccc0f313f1c422e699c
João Paulo Borges <joaopaulo.borges.gm@hotmail.com>
2022-09-04 23:01:42 -0300

    setting up auth swagger

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,26 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/', app, document);
+
+  const logger = new Logger('Bootstrap');
+
+  const port = process.env.PORT || 5000;
+
+  logger.log(`Auth Application starting using port ${port}`);
+  logger.log('Swagger Documentation available at /');
+  await app.listen(port);
 }
 bootstrap();
