b277ab5eedadea1f8267b8c1489248994311c1db
João Paulo Borges <joaopaulo.borges.gm@hotmail.com>
2022-09-06 15:53:05 -0300

    adding timestamps for user

--- apps/auth/src/users/user.entity.ts
+++ apps/auth/src/users/user.entity.ts
@@ -1,5 +1,11 @@
 import { Exclude } from 'class-transformer';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -15,4 +21,19 @@ export class User {
   @Column()
   @Exclude()
   password: string;
+
+  @CreateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  updatedAt: Date;
 }
